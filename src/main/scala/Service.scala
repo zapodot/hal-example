f410cc416f9d7fc615f0434647caebeaeaa7a1c9
Sondre Eikanger Kvalø <zapodot@gmail.com>
2013-10-16 19:56:29 +0200

    cha cha changes

--- src/main/scala/Service.scala
+++ src/main/scala/Service.scala
@@ -1,5 +1,6 @@
 import com.theoryinpractise.halbuilder.api.RepresentationFactory
 import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory
+import javax.servlet.http.HttpServletResponse
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import spark.{Filter, Response, Request, Route}
@@ -16,9 +17,13 @@ object Service extends App {
 
   setPort(8080)
 
+  staticFileLocation("browser")
+
   get(IndexRoute)
 
   get(UserListRoute)
+
+  get(UserItemRoute)
 
   after(new Filter() {
     def handle(request: Request, response: Response) = response.header("powered-by", "spark")
@@ -26,7 +31,11 @@ object Service extends App {
 
 }
 
-case class User(id: Long, name: String)
+case class User(id: Long, name: String) {
+  def getId = id
+
+  def getName = name
+}
 
 class IndexRoute(path: String) extends Route(path) {
   def handle(request: Request, response: Response): AnyRef = {
@@ -47,14 +56,13 @@ class UserListRoute(path: String) extends Route(path) {
 
   def handle(request: Request, response: Response): AnyRef = {
     import Service._
-    val resource = representationFactory.newRepresentation(path)
+    val resource = representationFactory.newRepresentation(path).withNamespace(Namespaces.user._1, Namespaces.user._2)
 
     userBuffer.foreach {
       user => {
         val userId = user.id
         resource.withRepresentation("users", representationFactory.newRepresentation(s"$path/$userId")
-          .withNamespace(Namespaces.user._1, Namespaces.user._2)
-          .withLink(Namespaces.user._1 + ":item",Paths.userItemPath )
+          .withLink(Namespaces.user._1 + ":item", Paths.userItemPath)
           .withProperty("id", user.id)
           .withProperty("name", user.name))
 
@@ -68,18 +76,44 @@ object UserListRoute extends UserListRoute(Paths.usersPath) {
   val representation = "users"
 }
 
-object UserItemRoute extends Route(Paths.usersPath) {
+object UserItemRoute extends Route(Paths.userItemPath) {
+
+  def extractIdFromRequest(request: Request): Option[Long] = {
+    request.params("id") match {
+      case id: String => Some(id.toLong)
+      case _ => None
+    }
+  }
+
+  def findUser(id: Long): Option[User] = Service.userBuffer find {
+    user => id == user.id
+  }
+
   def handle(request: Request, response: Response): AnyRef = {
 
+    val res = extractIdFromRequest(request) map {
+      userId => findUser(userId) match {
+        case None => {
+          response.status(HttpServletResponse.SC_NOT_FOUND)
+          s"No user found with id $userId"
+        }
+        case Some(user) => Service.representationFactory.newRepresentation(Paths.userItemPath.replace(":id", userId.toString))
+          .withBean(user)
+          .withLink("index", Paths.usersPath)
+          .toString(RepresentationFactory.HAL_JSON)
+      }
+
+    }
+    res.getOrElse("")
   }
 }
 
 object Paths {
   val usersPath = "/users"
-  val userItemPath = "/users/{id}"
+  val userItemPath = "/users/:id"
 }
 
 object Namespaces {
-  val user = ("user", "http://wiki.nextgentel.net/display/SYS/hal-elements#{rel}")
+  val user = ("user", "https://github.com/zapodot/hal-example/wiki#{rel}")
 }
 
